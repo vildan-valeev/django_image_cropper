e22e5d59797d1b3eeaa1c3085f4752a2093083ca
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2021-11-29 00:53:40 +0500

    add tests

--- src/app/tests.py
+++ src/app/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-# Create your tests here.
+from app.models import ImageLoad
+
+
+class ImageLoadTestCase(TestCase):
+    # def setUp(self):
+    #     pass
+
+    def test_create_image_load_instance(self):
+        new_instance = ImageLoad()
+        new_instance.title = 'test_image.jpg'
+        image_path = 'test_image.jpg'
+        new_instance.image = SimpleUploadedFile(name='test_image.jpg', content=open(image_path, 'rb').read(),
+                                                content_type='image/jpeg')
+        new_instance.save()
+        self.assertEqual(ImageLoad.objects.count(), 1)
+
